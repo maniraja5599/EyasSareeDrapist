b17f8ff6df87a3de612737cbf9ec2f909181df98
Eyas Admin <admin@eyasdrapist.com>
2026-01-24 16:17:16 +0530

    Remove unused Firebase deps to fix crash

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,4 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { auth } from '../firebase';
 
 const AuthContext = createContext({});
 
